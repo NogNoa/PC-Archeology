8943ad129840065aa98bf4c78daa93147d956fe8
NogNoa <53447424+NogNoa@users.noreply.github.com>
2025-05-22 19:34:44 +0300

    CGA: 4 pixel per byte

--- CGA.py
+++ CGA.py
@@ -2,9 +2,8 @@ import sys
 
 from PIL import Image
 
-image = Image.new("RGB", (0x20, 0x19))
-pixels = image.load()
-
+BG = (0, 0, 0xAA)
+GLOBAL_INTENSITY = 0x55
 
 def CGA_pallete(index: int) -> tuple[int, int, int]:
     index = index % 0x10
@@ -15,28 +14,36 @@ def CGA_pallete(index: int) -> tuple[int, int, int]:
             )
 
 
+def CGA_mode4_pallete_1(index: int) -> tuple[int, int, int]:
+    index = index % 4
+    if index == 0:
+        return BG
+    return (0xAA * bool(index & 2) + GLOBAL_INTENSITY,
+            0xAA * bool(index & 1) + GLOBAL_INTENSITY,
+            0xAA + GLOBAL_INTENSITY)
+
+
 def draw_low_middle(call: bytes):
-    pointer = 0x1424
     y = 0
-    while y < 0x18:
+    while call:
         x = 0
         for byte in call[:0x10]:
-            pix_couple = byte & 0xF, (byte & 0xF0) >> 4
-            pixels[x, y], pixels[x + 1, y] = (CGA_pallete(p) for p in pix_couple)
-            x += 2
-        pointer = pointer + 0x10
+            pix_quad = byte, byte >> 2, byte >> 4, byte >> 6
+            pix_quad = (p & 3 for p in pix_quad)
+            for pl, p in enumerate(pix_quad):
+                pixels[x + pl, y] = CGA_mode4_pallete_1(p)
+            x += 4
         call = call[0x10:]
-        x = 0
-        for byte in call[:0x10]:
-            pix_couple = byte & 0xF, byte & 0xF0 >> 4
-            pixels[x, y + 1], pixels[x + 1, y + 1] = pix_couple
-            x += 2
-        y += 2
+        y += 1
 
 
 scroll_nom = sys.argv[1]
 with open(scroll_nom, "rb") as file:
     scroll = file.read()
+
+image = Image.new("RGB", (0x40, len(scroll) // 0x10))
+pixels = image.load()
+
 draw_low_middle(scroll)
 
 image.save(f"{scroll_nom}.png")
