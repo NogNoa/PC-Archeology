3774189dd1bd598a145137584d8573d8380870c6
NogNoa <53447424+NogNoa@users.noreply.github.com>
2025-06-01 18:15:18 +0300

    CGA: draw_CG

--- CGA.py
+++ CGA.py
@@ -4,6 +4,9 @@ from PIL import Image
 
 BG = (0, 0, 0xAA)
 GLOBAL_INTENSITY = 0x55
+FIELD_SZ = 0x2000
+LINE_SZ = 320
+
 
 def CGA_pallete(index: int) -> tuple[int, int, int]:
     index = index % 0x10
@@ -37,13 +40,29 @@ def draw_low_middle(call: bytes):
         y += 1
 
 
+def draw_CG(call: bytes):
+    for pli, byte in enumerate(call):
+        x = pli * 4 % LINE_SZ
+        y = pli // LINE_SZ
+        pix_quad = byte >> 6, byte >> 4, byte >> 2, byte
+        pix_quad = (p & 3 for p in pix_quad)
+        for plj, p in enumerate(pix_quad):
+            pixels[x + plj, y] = CGA_mode4_pallete_1(p)
+
+
 scroll_nom = sys.argv[1]
 with open(scroll_nom, "rb") as file:
     scroll = file.read()
 
-image = Image.new("RGB", (0x80, len(scroll) // 0x20 + 1))
-pixels = image.load()
-
-draw_low_middle(scroll)
+if sys.argv[2] == "lm":
+    image = Image.new("RGB", (0x80, len(scroll) // 0x20 + 1))
+    pixels = image.load()
+    draw_low_middle(scroll)
+elif sys.argv[2] == "cg":
+    image = Image.new("RGB", (LINE_SZ, len(scroll) // LINE_SZ + 1))
+    pixels = image.load()
+    draw_CG(scroll)
+else:
+    raise ValueError
 
 image.save(f"{scroll_nom}.png")
