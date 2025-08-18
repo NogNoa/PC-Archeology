c65f10e90df570d111d0a190909f038d30f7b9b6
NogNoa <53447424+NogNoa@users.noreply.github.com>
2025-08-18 13:14:49 +0300

    don't have x drift to the right

--- src/CGA.py
+++ src/CGA.py
@@ -51,21 +51,20 @@ def draw_CG(call: bytes, width_pix, interlaced=True) -> Image.Image:
     else:
         fields = (call,)
     for field_i, field in enumerate(fields):
-        field = (field[y*line_sz:(y+1)*line_sz] for y in range(hight))
-        for y, line in enumerate(field):
+        field_gen = (field[y*line_sz:(y+1)*line_sz] for y in range(hight))
+        for y, line in enumerate(field_gen):
             if interlaced:
                 y = 2 * y + field_i
             for byte_i, byte in enumerate(line):
                 # each byte represents 4 pixels
-                x = 4 * byte_i
                 pix_quad = byte >> 6, byte >> 4, byte >> 2, byte
                 pix_quad = (p & 3 for p in pix_quad)
                 for pixel_i, p in enumerate(pix_quad):
-                    x = x + pixel_i
+                    x = 4 * byte_i + pixel_i
                     try:
                         pixels[x, y] = CGA_mode4_pallete_1(p)
                     except IndexError:
-                        print(f"Error: draw to [{x+pixel_i}, {y}]")
+                        print(f"Error: draw to [{x}, {y}]")
     return image
 
 
